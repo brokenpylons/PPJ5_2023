7281449ee279c7061650b8ebb23c8706c0bd70f1
Žiga Leber <zigaleber7@gmail.com>
2023-05-08 18:26:32 +0200

    Add examples

--- test.sh
+++ test.sh
@@ -9,13 +9,16 @@ declare -r path=$(dirname $(realpath "$0"))
 
 declare -i err=0
 
-for file in "$path"/positive/example{0..1000..10}.txt
+for file in "$path"/examples/example{0..20}.txt
 do
     echo "$file"
     cat "$file"
+    declare expected=$(cat "${file%.*}.expected")
     declare output=$(./run.sh "$file")
+    echo "$output"
+    echo "$expected"
 
-    if [[ "$output" == "accept" ]]
+    if [[ "$output" == "$expected" ]]
     then
         echo -e "\n[OK]"
     else
@@ -23,20 +26,3 @@ do
         ((err++))
     fi
 done
-
-for file in "$path"/negative/example{0..1000..10}.txt
-do
-    echo "$file"
-    cat "$file"
-    declare output=$(./run.sh "$file")
-
-    if [[ "$output" == "reject" ]]
-    then
-        echo -e "\n[OK]"
-    else
-        echo -e "\n[FAIL]"
-        ((err++))
-    fi
-done
-
-exit $err
